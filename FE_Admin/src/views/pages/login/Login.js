e95c3a92f854fed4b4e9449e5a9911fe111e13e6
phathaigithub <phathai2902@gmail.com>
2024-11-08 09:13:51 +0700

    login

--- FE_Admin/src/views/pages/login/Login.js
+++ FE_Admin/src/views/pages/login/Login.js
@@ -1,86 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {
-  CButton,
-  CCard,
-  CCardBody,
-  CCardGroup,
-  CCol,
-  CContainer,
-  CForm,
-  CFormInput,
-  CInputGroup,
-  CInputGroupText,
-  CRow,
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilLockLocked, cilUser } from '@coreui/icons'
+// src/pages/admin/LoginAdmin.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './LoginAdmin.css';
+import { CContainer, CSpinner } from '@coreui/react'
+// Import FontAwesomeIcon and specific icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 
-const Login = () => {
+function Login() {
   return (
-    <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md={8}>
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm>
-                    <h1>Login</h1>
-                    <p className="text-body-secondary">Sign In to your account</p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupText>
-                        <CIcon icon={cilUser} />
-                      </CInputGroupText>
-                      <CFormInput placeholder="Username" autoComplete="username" />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupText>
-                        <CIcon icon={cilLockLocked} />
-                      </CInputGroupText>
-                      <CFormInput
-                        type="password"
-                        placeholder="Password"
-                        autoComplete="current-password"
-                      />
-                    </CInputGroup>
-                    <CRow>
-                      <CCol xs={6}>
-                        <CButton color="primary" className="px-4">
-                          Login
-                        </CButton>
-                      </CCol>
-                      <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
-                        </CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Register Now!
-                      </CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
-          </CCol>
-        </CRow>
-      </CContainer>
-    </div>
-  )
+    <CContainer className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <div className="uf-form-signin">
+        <div className="text-center">
+          <h1 className="text-white h3">Account Login</h1>
+        </div>
+        <form className="mt-4">
+          <div className="input-group uf-input-group input-group-lg mb-3">
+            <span className="input-group-text">
+              <FontAwesomeIcon icon={faUser} />
+            </span>
+            <input type="text" className="form-control" placeholder="Username or Email address" />
+          </div>
+          <div className="input-group uf-input-group input-group-lg mb-3">
+            <span className="input-group-text">
+              <FontAwesomeIcon icon={faLock} />
+            </span>
+            <input type="password" className="form-control" placeholder="Password" />
+          </div>
+          <div className="d-flex mb-3 justify-content-between">
+            <Link to="/forgot-password" className="text-white">Forgot password?</Link>
+          </div>
+          <div className="d-grid mb-4">
+            <button type="submit" className="btn uf-btn-primary btn-lg">Login</button>
+          </div>
+          <div className="d-flex mb-3">
+          </div>
+        </form>
+      </div>
+    </CContainer>
+  );
 }
 
-export default Login
+export default Login;
